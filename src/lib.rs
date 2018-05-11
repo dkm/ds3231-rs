bcd96aa6ac43cb7e90c81fba569d6e090cfbaea7
Marc Poulhiès <dkm@kataplop.net>
2018-05-11 14:36:34 +0200

    Time handling
    Define struct for storing time.
    Define set/get function for reading/writing time to/from device.

--- src/lib.rs
+++ src/lib.rs
@@ -7,10 +7,11 @@
 #![feature(reverse_bits)]
 
 extern crate embedded_hal as hal;
+extern crate numtoa;
 
+use numtoa::NumToA;
 // #[macro_use(block)]
 // extern crate nb;
-
 
 /// Device descriptor
 #[derive(Clone, Copy, PartialEq)]
@@ -19,13 +20,47 @@ pub struct DS3231<S> {
     i2c : S,
 }
 
+/// Time struct
+#[derive(Clone, Copy, PartialEq, Default)]
+pub struct DS3231Time {
+    secs : u8,
+    mins : u8,
+    hours : u8,
+    wday : u8,
+    mday : u8,
+    month : u8,
+    year : u8
+}
+
+impl DS3231Time {
+    /// Returns simple string
+    pub fn get_simple(&self, buf : &mut [u8]) {
+        let mut digits = [0u8;4];
+
+        let s = self.mins.numtoa(10, &mut digits);
+        if s == 3 {
+            buf[1] = digits[s];
+        } else {
+            buf[0] = digits[s];
+            buf[1] = digits[s+1];
+        }
+
+        let s = self.secs.numtoa(10, &mut digits);
+        if s == 3 {
+            buf[3] = digits[s];
+        } else {
+            buf[2] = digits[s];
+            buf[3] = digits[s+1];
+        }
+    }
+}
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 enum DS3231Regs {
-    _Seconds = 0x0,
-    _Minutes = 0x1,
-    _Hours = 0x2,
-    _Day = 0x3,
+    Seconds = 0x0,
+    Minutes = 0x1,
+    Hours = 0x2,
+    Day = 0x3,
     _Date = 0x4,
     _MonthCentury = 0x5,
     _Year = 0x6,
@@ -54,19 +89,74 @@ impl<S> DS3231<S>
         }
     }
 
-    ///bob
-    pub fn get_temp(&mut self) -> (u8,u8) {
-        let temp_msb_reg = [ DS3231Regs::TempMsb as u8];
-        let temp_lsb_reg = [ DS3231Regs::TempLsb as u8];
-        let mut temp_buf = [0];
+    fn get_reg(&mut self, addr : DS3231Regs) -> u8 {
+        let addr_reg = [ addr as u8];
+        let mut buf = [0];
 
-        let val = match self.i2c.write_read(ADDR, &temp_msb_reg, &mut temp_buf) {
-            Ok(_) => temp_buf[0] as u8,
+        match self.i2c.write_read(ADDR, &addr_reg, &mut buf) {
+            Ok(_) => buf[0] as u8,
             Err(_) => 0u8
-        };
-        match self.i2c.write_read(ADDR, &temp_lsb_reg, &mut temp_buf) {
-            Ok(_) => (val, (temp_buf[0] >> 6) * 25),
-            Err(_) => (val, 0)
         }
+    }
+
+    fn set_reg(&mut self, addr : DS3231Regs, val: u8) {
+        let write_cmd = [ addr as u8, val];
+
+        // match i2c.write(ds3231_adr, &write_seconds) {
+        match self.i2c.write(ADDR, &write_cmd) {
+            Ok(_) => (),
+            Err(_) => (),
+        }
+    }
+
+
+    /// Returns the temperature as a tuple of (integer part, decimal part).
+    /// Decimal part begin one of 0,25 and 75.
+    pub fn get_temp(&mut self) -> (u8,u8) {
+        // let temp_msb_reg = [ DS3231Regs::TempMsb as u8];
+        // let temp_lsb_reg = [ DS3231Regs::TempLsb as u8];
+        // let mut temp_buf = [0];
+
+        (self.get_reg(DS3231Regs::TempMsb), self.get_reg(DS3231Regs::TempLsb))
+        // let val = match self.i2c.write_read(ADDR, &temp_msb_reg, &mut temp_buf) {
+        //     Ok(_) => temp_buf[0] as u8,
+        //     Err(_) => 0u8
+        // };
+        // match self.i2c.write_read(ADDR, &temp_lsb_reg, &mut temp_buf) {
+        //     Ok(_) => (val, (temp_buf[0] >> 6) * 25),
+        //     Err(_) => (val, 0)
+        // }
+    }
+
+    /// Returns the temperature as a float.
+    pub fn get_temp_float(&mut self) -> f32 {
+        let (int, dec) = self.get_temp();
+        int as f32 + dec as f32 / 100f32
+    }
+
+    /// Sets the time
+    pub fn set_time(&mut self, time : &DS3231Time) {
+        self.set_reg(DS3231Regs::Seconds, time.secs);
+        self.set_reg(DS3231Regs::Minutes, time.mins);
+        self.set_reg(DS3231Regs::Hours, time.hours);
+        self.set_reg(DS3231Regs::Day, time.wday);
+    }
+
+    /// Returns the time from device
+    pub fn get_time(&mut self) -> DS3231Time {
+        let mut ret_time : DS3231Time = Default::default();
+
+        let secs_val = self.get_reg(DS3231Regs::Seconds);
+        ret_time.secs = (secs_val & 0xF) + (10 * (secs_val>>4));
+
+        let mins_val = self.get_reg(DS3231Regs::Minutes);
+        ret_time.mins = (mins_val & 0xF) + (10 * (mins_val>>4));
+
+        let hours_val = self.get_reg(DS3231Regs::Hours);
+        ret_time.hours = (hours_val & 0xF) + (10 * (hours_val>>4));
+
+        ret_time.wday = self.get_reg(DS3231Regs::Day);
+
+        ret_time
     }
 }
